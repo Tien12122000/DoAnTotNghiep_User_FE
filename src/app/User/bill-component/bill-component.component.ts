7038a2caf514d2cee6ba1ab4250f37ad700fb073
Tien12122000 <106265875+Tien12122000@users.noreply.github.com>
2022-05-26 09:30:03 +0700

    first commit

---
+++ src/app/User/bill-component/bill-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-component',
+  templateUrl: './bill-component.component.html',
+  styleUrls: ['./bill-component.component.css']
+})
+export class BillComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './User/header/header.component';
+import { FooterComponent } from './User/footer/footer.component';
+import { BaseComponent } from './core/base/base.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CartComponent } from './User/cart/cart.component';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { BillComponentComponent } from './User/bill-component/bill-component.component';
+// import { ProductDeltailComponent } from './User/product-deltail/product-deltail.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BaseComponent,
+    CartComponent,
+    BillComponentComponent,
+    // ProductDeltailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NzNotificationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
